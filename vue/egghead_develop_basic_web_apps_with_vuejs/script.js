82511e6bcfda5231375ff7933cdd345a9e068c91
richard <richardroygibson@gmail.com>
2020-11-15 12:12:53 +1100

    video 5, introduced watch

--- vue/egghead_develop_basic_web_apps_with_vuejs/script.js
+++ vue/egghead_develop_basic_web_apps_with_vuejs/script.js
@@ -3,41 +3,27 @@ var card = new Vue({
   data: {
     title: "Dinosaurs",
     input: "",
-    speciesUpdated: 0,
-    dinosUpdated: 0,
-    items: [
-      { text: "Tyrannosaurus", quantity: 5 },
-      { text: "Triceratops", quantity: 3 },
-      { text: "Stegosaurus", quantity: 6 }
-    ]
+    buttonText: "Add Dinosaur",
+    items: ["Tyrannosaurus", "Triceratops", "Stegosaurus"]
   },
   computed: {
-    totalDinos: function() {
-      this.dinosUpdated += 1;
-      var sum = 0;
-      var items = this.items;
-
-      for (var i in items) {
-        sum += items[i].quantity;
-      }
-
-      return sum;
-    },
-    totalSpecies: function() {
-      this.speciesUpdated += 1;
-      return this.items.length;
+    buttonDisabled: function() {
+      return this.input == "";
     }
   },
+  watch: {
+    input: _.debounce(function() {
+      this.buttonText =
+        this.input !== "" ? "Add " + this.input : "Add Dinosaur";
+    }, 250)
+  },
   methods: {
-    addItem: function(e) {
-      e.preventDefault();
-      if (!this.input) return;
+    addItem: function() {
+      if (this.input !== "") {
+        this.items.push(this.input);
 
-      this.items.push({ text: this.input, quantity: 1 });
-      this.input = "";
-    },
-    removeItem: function(item) {
-      this.items.splice(item, 1);
+        this.input = "";
+      }
     }
   }
 });
